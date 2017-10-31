582883a76e708fc5e0c8d3bb7fe21e99bc97adc3
CodeFace <i@codeface.cn>
2017-10-31 11:05:27 +0800

    add run_testnet.sh

--- run_testnet.sh
+++ run_testnet.sh
@@ -5,8 +5,8 @@ ulimit -n 65535
 
 export COIN=Qtum
 export DAEMON_URL=http://user:password@127.0.0.1
-export NET=mainnet
-export DB_DIRECTORY=$HOME/.electrumx/db
+export NET=testnet
+export DB_DIRECTORY=$HOME/.electrumx/testnet_db
 export HOST=0.0.0.0
 export SSL_KEYFILE=$HOME/.electrumx/keyfile.key
 export SSL_CERTFILE=$HOME/.electrumx/certfile.crt
