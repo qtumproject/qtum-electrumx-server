50bddc6838b07d5a0b8ecfe71ca4e31b42dcd3d0
CodeFace <cnchenjun@qq.com>
2019-05-13 16:00:44 +0800

    update quick_run.sh

--- quick_run.sh
+++ quick_run.sh
@@ -11,10 +11,7 @@ export NET=mainnet
 export DB_DIRECTORY=$HOME/.electrumx/db
 export SSL_CERTFILE=$HOME/.electrumx/certfile.crt
 export SSL_KEYFILE=$HOME/.electrumx/keyfile.key
-export HOST=127.0.0.1
-export TCP_PORT=50001
-export SSL_PORT=50002
-
+export SERVICES=tcp://127.0.0.1:50001
 ulimit -n 65535
 
 
